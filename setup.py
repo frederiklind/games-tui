ca2e40de66ba3c7c38c842b02c4a0e8265d357c8
Frederik Lind <frederik.lind.mail@gmail.com>
2025-02-25 18:24:51 +0100

    changes

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import shutil
 import argparse
 import platform
 
@@ -25,9 +26,9 @@ def get_config_dir(user_os: str, app_name: str) -> str:
         case "Darwin":
             home = os.environ.get("HOME", os.getcwd())
             return os.path.join(home, "Library", "Preferences", app_name)
-        case "Windows":
-            appdata = os.environ.get("APPDATA", os.getcwd())
-            return os.path.join(appdata, app_name)
+        # case "Windows":
+        #     appdata = os.environ.get("APPDATA", os.getcwd())
+        #     return os.path.join(appdata, app_name)
         case _:
             return os.path.join(os.getcwd())
 
@@ -51,16 +52,68 @@ def get_data_dir(user_os: str, app_name: str) -> str:
         case "Darwin":
             home = os.environ.get("HOME", os.getcwd())
             return os.path.join(home, "Library", "Application Support")
-        case "Windows":
-            
+        # case "Windows":
+        case _:
+            return os.path.join(os.getcwd())
+                        
 
+def main() -> None:
+    """
 
-def main():
+    """
+    print("Installing rubiks-tui:\n")
+
+    app_name = "rubiks-tui"
     user_os = platform.system()
-
+    print(f"Operating syste: {user_os}")
     
+    config_dir = get_config_dir(user_os, app_name)
+    data_dir = get_data_dir(user_os, app_name)
+    
+    # setup config directory
+    if not os.path.exists(config_dir):
+        config_dirs = [
+            config_dir,
+            os.path.join(config_dir, "ascii"),
+            os.path.join(config_dir, "colorschemes")
+        ]
+        os.makedirs(config_dirs)
+        for dir in config_dirs:
+            os.makedirs(dir)
+            print(f"> Created directory: {dir}")
 
+        # copy ascii banners
+        ascii_src = os.path.join(os.getcwd(), "conf", "ascii")
+        for file in os.listdir(ascii_src):
+            src_file = os.path.join(ascii_src, file)
+            dest_file = os.path.join(config_dirs[1], file)
+            if os.path.isfile(src_file):
+                shutil.copy2(src_file, dest_file)
+                print(f"> Copied {file} to {dest_file}")
+        
+        clrs_src = os.path.join(os.getcwd, "conf", "colorschemes")
+        for file in os.listdir(clrs_src):
+            src_file = os.path.join(clrs_src, file)
+            dest_file = os.path.join(config_dirs[2], file)
+            if os.path.isfile(src_file):
+                shutil.copy2(src_file, dest_file)
+                print(f"> Copied {file} to {dest_file}")
 
+        conf_src = os.path.join(os.getcwd(), "conf", "config.toml")
+        conf_dest = os.path.join(config_dir, "config.toml")
+        shutil.copy2(conf_src, conf_dest)
+        print("> Copied default configuration to config directory.")
+    else:
+        print(f"> Config directory already exist. Skipping this step")
+    
+    # setup data directory
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+        print(f"> Created directory: {data_dir}")
+    else:
+        print("> Data directory already exist. Skipping this step")
+    
+    print("Installation finished!") 
 
 
 if __name__ == "__main__":
